504830a01c993b83f3c07d3aa30b5174f3f52a34
James Dinsdale <hi@molovo.co.uk>
2016-01-10 21:27:49 +0000

    Add molovo/object dependency, and mod Config to extend it

--- src/Config.php
+++ src/Config.php
@@ -3,86 +3,10 @@
 namespace Molovo\Interrogate;
 
 use Molovo\Interrogate\Database;
+use Molovo\Object\Object;
 
-class Config
+class Config extends Object
 {
-    /**
-     * The stored config values.
-     *
-     * @var stdClass
-     */
-    private $values = null;
-
-    /**
-     * Create a new Config object.
-     *
-     * @param array $values The config values
-     */
-    public function __construct(array $values = [])
-    {
-        foreach ($values as &$value) {
-            if (is_array($value)) {
-                $value = new self($value);
-            }
-        }
-
-        $this->values = (object) $values;
-    }
-
-    /**
-     * Get a config value.
-     *
-     * @param string $key The key of the value to get
-     *
-     * @return mixed The value
-     */
-    public function __get($key)
-    {
-        if (isset($this->values->{$key})) {
-            return $this->values->{$key};
-        }
-
-        if ($value = $this->valueForPath($key)) {
-            return $value;
-        }
-
-        return;
-    }
-
-    /**
-     * Return the config as an array.
-     *
-     * @return array
-     */
-    public function toArray()
-    {
-        return (array) $this->values;
-    }
-
-    /**
-     * Get a value for a nested path.
-     *
-     * @param string $path The path to fetch
-     *
-     * @return mixed The value
-     */
-    public function valueForPath($path)
-    {
-        $bits = explode('.', $path);
-
-        $value = $this->values;
-        foreach ($bits as $bit) {
-            if (is_object($value)) {
-                $value = $value->{$bit};
-                continue;
-            }
-
-            return;
-        }
-
-        return $value;
-    }
-
     /**
      * Get a config value.
      *
@@ -93,5 +17,18 @@ class Config
     public static function get($key)
     {
         return Database::config()->valueForPath($key);
+    }
+
+    /**
+     * Set a config value.
+     *
+     * @param string $path  The path of the value to set
+     * @param mixed  $value The value to set
+     *
+     * @return mixed The value
+     */
+    public static function set($key, $value = null)
+    {
+        return Database::config()->setValueForPath($key, $value);
     }
 }
